6612aec17017eff61c59c3da35a2332e717926ec
gauri dadhich <gauridadhich16.09@gmail.com>
2024-09-05 19:49:36 +0530

    hey

--- index.js
+++
@@ -1,7 +0,0 @@
-burger=document.querySelector('.burger')
-navbarItems=document.querySelector('.navbar')
-nav=document.querySelector('.items')
-burger.addEventListener('click',()=>{
-    navbarItems.classList.toggle('h-class')
-    nav.classList.toggle('v-class')
-})
